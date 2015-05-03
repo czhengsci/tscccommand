946b1e5c2c4393b834251b0790e27314a5e585e4
czhengsci <czhengsci@gmail.com>
2015-05-03 16:18:01 -0700

    start Low_Energy_Analysis

---
+++ Low_Energy_Analysis.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+__author__ = 'chenzheng'
+
+"""
+A master script Chen used for generating vasp output analysis to CSV
+"""
+
+
+import argparse
+import os
+import re
+
+
+
+
+
+
+def get_energies():
+    pass
+
+
+
+
+def get_magnetization():
+    pass
+
+
+
+def get_ave_magnetization():
+    pass
+
+
+def get_incar_para():
+    pass
+
+
+
+
+
+
