b814da431e1b11c1fa322f8655db22d5217ecbb1
czhengsci <czhengsci@gmail.com>
2015-05-08 15:36:20 -0700

    add and modified Job Restore

--- Job_Restore.py
+++ Job_Restore.py
@@ -10,20 +10,41 @@ __date__ = "Jan 18, 2015"
 import argparse
 import os,glob
 import subprocess
-import monty
+from monty.os import cd
 import logging
+
+
+def submit_command(args,entry):
+    var_dic = vars(args)
+
+    logging.debug(var_dic)
+    logging.debug('Wall time is none is: {}'.format(var_dic['walltime']==None))
+
+    subcommand = 'qsend_multi {} -q {} -v {} -c {} -n {} -nod {} -ib {} -w {}'.format(entry,var_dic['queue'],
+                                                                                        var_dic['verbosity'], var_dic['command'],
+                                                                                        var_dic['name'], var_dic['nnodes'],
+                                                                                        var_dic['ibswitch'],var_dic['walltime'])
+    logging.debug('Resub command is: \n {}'.format(subcommand))
+    
+    return subcommand
+
+
 
 #This is a funciton we used for check, restore and resub job using PBS system
 #Integrate with check_CONTCARRElax command
 def check_resub_relax2(args):
 
+
+
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
+
+    submit_command(args)
 
     dir_entry = parse_entry(args)
     for ele in dir_entry:
 
-        with monty.os.cd(ele):
+        with cd(ele):
 
             logging.debug('Current dir path is: {}'.format(os.getcwd()))
 
@@ -39,22 +60,28 @@ def check_resub_relax2(args):
                 subprocess.call(check_command,shell=True)
 
             no_relax2_file = [f for f in os.listdir('.') if f not in exclude]
+
+            for dir_ele in no_relax2_file:
+                resub_command = submit_command(dir_ele)
+                subprocess.call(resub_command,shell=True)
+
+
             logging.debug('Folder with no_relax2_file is: {}'.format(no_relax2_file))
 
-    pass
+
 
 
 #Function for parsing directories and return with target directories entry to be analysis
 def parse_entry(args):
 
-    rootpath = args.dir[0].rstrip('/')
+    rootpath = args.directories[0].rstrip('/')
     pathdepth = glob.glob(rootpath+'/*'*int(args.depth))
-    dirlistdepth = filter(lambda f: os.path.isdir(f),pathdepth)
+    subdir_list = filter(lambda f: os.path.isdir(f),pathdepth)
 
-    logging.debug('The directory contained in the path directory is: {}'.format(dirlistdepth))
-    # logging.debug('The type of dirlistdepth parameter is:{}'.format(type(dirlistdepth)))
+    logging.debug('The directory contained in the path directory is: {}'.format(subdir_list))
+    # logging.debug('The type of subdir_list parameter is:{}'.format(type(subdir_list)))
 
-    return dirlistdepth
+    return subdir_list
 
 
 def main():
@@ -64,14 +91,15 @@ def main():
     several sub-commands with their own options. To see the options for the
     sub-commands, type "pmg sub-command -h".""",
                                      epilog="""
-    Author: Shyue Ping Ong
+    Author: Chen Zheng
     Version: {}
     Last updated: {}""".format(__version__, __date__))
 
     subparsers = parser.add_subparsers()
 
-    parser_resub = subparsers.add_parser("relax_resub", help="Resub job based on existing of relax2 file")
-    parser_resub.add_argument("directories", metavar="dir", dest='dir',default=".",
+    parser_resub = subparsers.add_parser("relaxresub", help="Resub job based on existing of relax2 file.")
+
+    parser_resub.add_argument("directories", metavar="dir", default=".",
                              type=str, nargs="*",
                              help="directory to process (default to .)")
     parser_resub.add_argument("-dep",  "--depth", dest='depth', default="1",
@@ -87,11 +115,43 @@ def main():
                               default='No',help="Option to move folder that does contain CONTCAR.Relax2.gz to \
                               other directory specified in argument storedirectory")
 
+    parser_resub.add_argument("-q", "--queue", dest="queue", type=str,
+                                nargs="?", default="glean",
+                                choices=["home", "condo", "glean",'hotel'],
+                                help="Queue to send jobs to. Defaults to 'glean'. "
+                                     "Other option is 'condo' or 'home' or 'hotel'.")
+    parser_resub.add_argument("-v", "--verbosity", dest="verbosity", type=str,
+                                nargs="?", default="bae", choices=["ae", "bae"],
+                                help="Verbosity of the run. Standard PBS "
+                                     "notification settings. Options are a (abort "
+                                     "only - the default), ae (abort and end) and bae ("
+                                     "begin, abort and end).")
+    parser_resub.add_argument("-c", "--command", dest="command", type=str,
+                                nargs="?", default="run_vasp",
+                                choices=["run_vasp", "run_vasp_md","run_vasp_glean",'run_vasp_single'],
+                                help="Command to run. Defaults to 'vasp'.")
+
+    parser_resub.add_argument("-n", "--name", dest="name", type=str,
+                                nargs="?", default="jobs",
+                                help="Name for your jobs. Makes it easier to "
+                                     "identify which series of jobs they "
+                                     "belong to. Keep it short and crytic.")
+    parser_resub.add_argument('-nod','--numnodes',dest='nnodes',type=int,
+                                nargs="?",default='1',help='Command to setup number of nodes.')
+
+    parser_resub.add_argument("-ib", "--ibswitch", dest="ibswitch", type=str,
+                                choices=['ib','ib:ibswitch3','ib:ibswitch4','ib:rack1','ib:rack2'],
+                                nargs="?", default="ib",
+                                help="Command to setup ibswitch. Defaults to 'ib'")
+
+    parser_resub.add_argument("-w", "--walltime", dest="walltime", type=int,
+                                nargs="?", default=None,
+                                help="Provide a walltime for the job in hours. "
+                                     "Defaults are specified for each queue. But for "
+                                     "the hotel queue, you must specify a walltime.")
+
+
     parser_resub.set_defaults(func=check_resub_relax2)
-
-
-
-
 
 
     args = parser.parse_args()
